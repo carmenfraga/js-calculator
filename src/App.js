09a04f3d9c22c1a0b016827d0332751f97bd6c76
Carmen Fraga <carmenfragad@gmail.com>
2022-09-06 02:02:06 +0200

    calculator ending and styles

--- src/App.js
+++ src/App.js
@@ -1,57 +1,140 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
-
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [displayPrimaryValue, setDisplayPrimaryValue] = useState("0");
+  const [displaySecondaryValue, setDisplaySecondaryValue] = useState("");
+  const [total, setTotal] = useState(0);
+  const [isEqualsClicked, setIsEqualsClicked] = useState(false);
 
-const [displayPrimaryValue, setDisplayPrimaryValue] = useState(0)
-const [displaySecondaryValue, setDisplaySecondaryValue] = useState('')
+  const handleClearClick = () => {
+    setDisplayPrimaryValue("0");
+    setDisplaySecondaryValue("");
+    setIsEqualsClicked(false);
+  };
 
-const handleClearClick = () => {
-  setDisplayPrimaryValue(0)
-  setDisplaySecondaryValue('')
-}
+  const handleNumberClick = (event) => {
+    const numberClicked = event.target.value;
 
-const handleNumberClick = (event) => {
-  const numberClicked = event.target.value
-  const newValue = displayPrimaryValue === 0 ? numberClicked : displayPrimaryValue + numberClicked
-  const secondaryValue = displaySecondaryValue + numberClicked
-  setDisplayPrimaryValue(eval(secondaryValue))
-  setDisplaySecondaryValue(secondaryValue)
-}
+    if (isEqualsClicked) {
+      setDisplayPrimaryValue(numberClicked);
+      setDisplaySecondaryValue(numberClicked);
+      setTotal(numberClicked);
+      setIsEqualsClicked(false);
+      return;
+    }
 
-const handleOperatorClick = (event) => {
-  const operatorClicked = event.target.value
+    const primaryValue =
+      displayPrimaryValue === "0"
+        ? numberClicked
+        : displayPrimaryValue + numberClicked;
 
-  setDisplaySecondaryValue(displaySecondaryValue + operatorClicked)
-  
-}
+    const secondaryValue =
+      displaySecondaryValue === "0"
+        ? numberClicked
+        : displaySecondaryValue + numberClicked;
+
+    setTotal(eval(secondaryValue));
+    setDisplayPrimaryValue(primaryValue);
+    setDisplaySecondaryValue(secondaryValue);
+    setIsEqualsClicked(false);
+  };
+
+  const handleOperatorClick = (event) => {
+    const operatorClicked = event.target.value;
+
+    setDisplayPrimaryValue("0");
+    setDisplaySecondaryValue(displaySecondaryValue + operatorClicked);
+    setIsEqualsClicked(false);
+  };
+
+  const handleEqualsClick = () => {
+    setDisplayPrimaryValue(total);
+    setIsEqualsClicked(true);
+  };
 
   return (
     <div className="App">
       <h1>My Javascript Calculator</h1>
-      <button id="equals">=</button>
-      <button id="zero" onClick={handleNumberClick} value="0">0</button>
-      <button id="one" onClick={handleNumberClick} value="1">1</button>
-      <button id="two" onClick={handleNumberClick} value="2">2</button>
-      <button id="three" onClick={handleNumberClick} value="3">3</button>
-      <button id="four" onClick={handleNumberClick} value="4">4</button>
-      <button id="five" onClick={handleNumberClick} value="5">5</button>
-      <button id="six" onClick={handleNumberClick} value="6">6</button>
-      <button id="seven" onClick={handleNumberClick} value="7">7</button>
-      <button id="eight" onClick={handleNumberClick} value="8">8</button>
-      <button id="nine" onClick={handleNumberClick} value="9">9</button>
-      <button id="add" onClick={handleOperatorClick} value="+">+</button>
-      <button id="subtract" onClick={handleOperatorClick} value="-">-</button>
-      <button id="multiply" onClick={handleOperatorClick} value="*">X</button>
-      <button id="divide" onClick={handleOperatorClick} value="/">/</button>
-      <button id="decimal">.</button>
-      <button id="clear" onClick={handleClearClick}>AC</button>
-      <div id="displaySecondary">{displaySecondaryValue}</div>
-      <div id="display">{displayPrimaryValue}</div>
 
-
-</div>
+      <div id="calculator-box">
+        <div class="display" id="displaySecondary">
+          {displaySecondaryValue}
+        </div>
+        <div class="display" id="display">
+          {displayPrimaryValue}
+        </div>
+        <button id="clear" onClick={handleClearClick}>
+          AC
+        </button>
+        <button
+          class="operator-button"
+          id="divide"
+          onClick={handleOperatorClick}
+          value="/"
+        >
+          /
+        </button>
+        <button
+          class="operator-button"
+          id="multiply"
+          onClick={handleOperatorClick}
+          value="*"
+        >
+          X
+        </button>
+        <button
+          class="operator-button"
+          id="add"
+          onClick={handleOperatorClick}
+          value="+"
+        >
+          +
+        </button>
+        <button id="seven" onClick={handleNumberClick} value="7">
+          7
+        </button>
+        <button id="eight" onClick={handleNumberClick} value="8">
+          8
+        </button>
+        <button id="nine" onClick={handleNumberClick} value="9">
+          9
+        </button>
+        <button
+          class="operator-button"
+          id="subtract"
+          onClick={handleOperatorClick}
+          value="-"
+        >
+          -
+        </button>
+        <button id="four" onClick={handleNumberClick} value="4">
+          4
+        </button>
+        <button id="five" onClick={handleNumberClick} value="5">
+          5
+        </button>
+        <button id="six" onClick={handleNumberClick} value="6">
+          6
+        </button>
+        <button id="equals" onClick={handleEqualsClick}>
+          =
+        </button>
+        <button id="one" onClick={handleNumberClick} value="1">
+          1
+        </button>
+        <button id="two" onClick={handleNumberClick} value="2">
+          2
+        </button>
+        <button id="three" onClick={handleNumberClick} value="3">
+          3
+        </button>
+        <button id="zero" onClick={handleNumberClick} value="0">
+          0
+        </button>
+      </div>
+      <div id="calculator-buttons"></div>
+    </div>
   );
 }
 
